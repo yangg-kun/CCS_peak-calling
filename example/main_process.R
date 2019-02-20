1ce51e96fce05b610944f43a4abcf7b0d58e2334
kyang <1627528922@qq.com>
2019-02-20 14:41:57 +0800

    more comments in main_process.R

--- example/main_process.R
+++ example/main_process.R
@@ -12,24 +12,24 @@ src_path <- function(path) paste0(src_dir, path)
 if(! "here" %in% installed.packages()) install.packages("here")
 require(here)
 
-
-## scanning
-model_file <- "./trained_model_50bp.RData"
-n_cores <- 8 ## number of cores used in parallel process
+## main processes
+### scanning
+model_file <- "./trained_model_50bp.RData"  # including model variable 'trained_model'
+n_cores <- 8  # number of cores used in parallel process
 window_size <- 50
 step_size <- 1
-fa_file <- "./example.fa"
-pred_file <- "./example.pred"
+fa_file <- "./example.fa"  # input fasta file
+pred_file <- "./example.pred"  # path for saving predicted results
 source(src_path("scanning.R"))
 
-## create a table of predictions with position information
+### creating a table of predictions with position information from scanning result
 predictions <- "./example.pred"
 saving <- "./example.ppred"
 source(src_path("title2position.R"))
 
-## peak-calling
-predictions <- "./example.ppred"
-mismatch <- 3 ## maximal of mismatches
-consistency <- 0.8 ## proportion of consistency
-peak_file <- "./example.peak"
+### peak-calling
+predictions <- "./example.ppred"  # input file
+mismatch <- 3  # maximal of mismatches
+consistency <- 0.8  # proportion of consistency
+peak_file <- "./example.peak"  # path for saving detected peaks
 source(src_path("peak_calling.R"))
