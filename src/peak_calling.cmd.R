2381bcf97283a7baa7acfde0e6ea1745fd865ee7
kyang <1627528922@qq.com>
2019-02-20 15:03:14 +0800

    change parameter name in peak_calling.cmd.R

--- src/peak_calling.cmd.R
+++ src/peak_calling.cmd.R
@@ -14,7 +14,7 @@ spec = matrix(c(
   'mismatch', 'm', 2, "integer", "maximal of mismatches",
   'consistency', 'c', 2, "double", "proportion of consistency",
   'output', 'o', 1, "character", "path for saving selected peaks",
-  'title2position', 't', 2, "character", "transforming predictions into required form",
+  'transformation', 't', 2, "character", "path for saving transformed table of predictions",
   'help', 'h', 0, "logical", "help for peak-calling"
 ), byrow = T, ncol = 5)
 
@@ -28,15 +28,15 @@ if(is.null(opt$input)){opt$input = "./example/example.pred"}
 if(is.null(opt$mismatch)){opt$mismatch = 3}
 if(is.null(opt$consistency)){opt$consistency = 0.8}
 if(is.null(opt$output)){opt$output = "./example/example.peak"}
-if(is.null(opt$title2position)){opt$title2position = ""}
+if(is.null(opt$transformation)){opt$transformation = ""}
 
 predictions <- opt$input
 mismatch <- opt$mismatch
 consistency <- opt$consistency
 peak_file <- opt$output
 
-if(nchar(opt$title2position) > 0){
-  saving <- opt$title2position
+if(nchar(opt$transformation) > 0){
+  saving <- opt$transformation
   source(here("title2position.R"))
   predictions <- saving
 }
--- src/peak_calling_functions.R
+++ src/peak_calling_functions.R
@@ -123,3 +123,68 @@ count_trend <- function(vec, point=1, direction=1,
        total = count_total, 
        per = round(count_true / count_total, 2))
 }
+
+get_peaks <- function(values, top_min = 3, peak_min = 2, 
+                      tolerance = 3, reset = T)
+{
+  peaks <- data.frame()
+  tops <- get_top_points(values, minimun = top_min)
+  ind <- which(values < peak_min)
+  for(ti in tops){
+    sta_ti <- max(ind[ind < ti]) + 1
+    end_ti <- min(ind[ind > ti]) - 1
+    if(is.na(sta_ti) | sta_ti > ti){sta_ti <- 1}
+    if(is.na(end_ti) | end_ti < ti){end_ti <- len(values)}
+    val_ti <- values[sta_ti:end_ti]
+    ii <- ti - sta_ti + 1
+    peaks <- rbind(peaks, 
+                   count_trend(val_ti, ii, 
+                               direction = -1, 
+                               tolerance = tolerance, 
+                               reset = reset) %>% 
+                     as.df() %>% cbind(top_p=ti, .), 
+                   count_trend(val_ti, ii, 
+                               direction = 1, 
+                               tolerance = tolerance, 
+                               reset = reset) %>% 
+                     as.df() %>% cbind(top_p=ti, .))
+  }
+  peaks %>% 
+    mutate(end_p = top_p - start_at + end_at) %>% 
+    select(top_p, count, total, end_p) %>% 
+    group_by(top_p) %>% 
+    summarise(count = sum(count), 
+              total = sum(total), 
+              start = min(end_p), 
+              end = max(end_p)) %>% 
+    ungroup %>% 
+    rename(top = top_p) %>% 
+    mutate(percent = count / total)
+}
+
+## peaks from a data.frame of several discontinuous regions
+df_get_peaks <- function(df, mismatch=0)
+{
+  regs <- df %>% arrange(chr, position) %>%
+    select(position) %>% unlist() %>% reg_vec()
+  peaks <- data.frame()
+  for(ri in 1:(nrow(regs))){
+    df_ri <- df %>% 
+      slice(regs$i_start[ri]:regs$i_end[ri]) %>% 
+      df_get_region()
+    p_ri <- df_ri$value %>% 
+      get_peaks(tolerance = mismatch)
+    if(nrow(p_ri) == 0) next
+    p_ri <- p_ri %>% 
+      mutate(chr = df_ri$chr[top]) %>% 
+      mutate(start = df_ri$position[start]) %>% 
+      mutate(end = df_ri$position[end]) %>% 
+      mutate(top = df_ri$position[top]) %>% 
+      df_get_region(remove = T) %>% 
+      select(region, everything()) %>% 
+      rename(peak = region)
+    peaks <- peaks %>% 
+      rbind(p_ri %>% cbind(region=ri, .))
+  }
+  peaks
+}
